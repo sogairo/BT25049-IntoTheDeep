31eafdec56a8cf4c46b557feaee8086e7b49962d
sogairo <seishichirou@gmail.com>
2025-01-09 00:08:40 -0500

    Updated DriveTeleOp
    - Integrated PID for Spool Motors

--- TeleOp/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleOp.java
+++ TeleOp/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleOp.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -9,73 +12,50 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "BT25046-Drive", group = "Competition")
-public class UpdatedDriveProgram extends LinearOpMode {
+public class DriveTeleOp extends LinearOpMode {
     // PID Setup
     private PIDController controller;
+    public double p = 0, i = 0, d = 0;
+    public double f = 0;
 
-    public static double p = 0, i = 0, d = 0;
-    public static double f = 0;
-    public static int target = 0;
-    private final double ticks_in_degree = 700 / 180.0;
+    public double CalculatePIDPower(DcMotor Motor, int Target) {
+        final double feedForwardConstant = 700 / 180.0;
 
-    // Initialize Constants
-    final double MOTOR_POWER_CONSTRAINT = 0.65;
+        controller.setPID(p, i, d);
+        int leftSpoolPosition = Motor.getCurrentPosition();
+        double PID = controller.calculate(leftSpoolPosition, Target);
+        double feedForward = Math.cos(Math.toRadians(Target / feedForwardConstant)) * f;
 
-    final double SPOOL_MOTOR_POWER = 1.0;
-    final double STATIONARY_SPOOL_POWER = 0.0001; // Change this value if gravity is a higher force
-
-    final int MAX_SPOOL_POSITION = 1620; // change later
-    final int MIN_SPOOL_POSITION = 0; // change later
-    final int SPOOL_INCREMENT = 10; // change later
-
-    final double MAX_ARM_POSITION = 1;
-    final double MIN_ARM_POSITION = 0;
-    final double ARM_INCREMENT = 0.02; // change later
-
-    // Initialize Variables
-    int currentSpoolPosition = MIN_SPOOL_POSITION;
-    double currentArmPosition = MIN_ARM_POSITION;
-
-    boolean pressXDebounce = false;
-    boolean pressYDebounce = false;
-
-    @Override
-    public void init() {
-
+        return PID + feedForward;
     }
 
-
+    // On Initialize
     @Override
     public void runOpMode() throws InterruptedException {
-        /*
         // Initialize Constants
         final double MOTOR_POWER_CONSTRAINT = 0.65;
 
         final double SPOOL_MOTOR_POWER = 1.0;
-        final double STATIONARY_SPOOL_POWER = 0.0001; // Change this value if gravity is a higher force
 
-        final int MAX_SPOOL_POSITION = 1620; // change later
-        final int MIN_SPOOL_POSITION = 0; // change later
-        final int SPOOL_INCREMENT = 10; // change later
+        final int MAX_SPOOL_POSITION = 1620;
+        final int MIN_SPOOL_POSITION = 0;
+        final int SPOOL_INCREMENT = 10; // Need adjustments?
 
         final double MAX_ARM_POSITION = 1;
         final double MIN_ARM_POSITION = 0;
-        final double ARM_INCREMENT = 0.02; // change later
-        */
+        final double ARM_INCREMENT = 0.02;
 
-        /*
-        // Initialize Variables
+        // Initialize Variables.
         int currentSpoolPosition = MIN_SPOOL_POSITION;
         double currentArmPosition = MIN_ARM_POSITION;
 
         boolean pressXDebounce = false;
         boolean pressYDebounce = false;
-        boolean pressBumperDebounce = false;
-        */
 
-        // Initialize Motors, Servos, Sensors
+        controller = new PIDController(p, i ,d);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        // Drivetrain Configuration
+        // Drivetrain Configuration.
         DcMotor topLeftMotor = hardwareMap.dcMotor.get("topLeft");
         DcMotor bottomLeftMotor = hardwareMap.dcMotor.get("bottomLeft");
 
@@ -85,19 +65,19 @@ public class UpdatedDriveProgram extends LinearOpMode {
         DcMotor bottomRightMotor = hardwareMap.dcMotor.get("bottomRight");
         bottomRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        // Claw Configuration
+        // Claw Configuration.
         Servo remadeClawServo = hardwareMap.servo.get("remadeClaw");
 
-        // Claw Pivot Configuration
+        // Claw Pivot Configuration.
         Servo clawPivot = hardwareMap.servo.get("clawPivot");
 
-        // Arm Configuration
+        // Arm Configuration.
         Servo armRightServo = hardwareMap.servo.get("armRight");
         armRightServo.setDirection(Servo.Direction.REVERSE);
 
         Servo armLeftServo = hardwareMap.servo.get("armLeft");
 
-        // Spool Linear Slides Configuration
+        // Spool Linear Slides Configuration.
         DcMotor spoolLeftMotor = hardwareMap.dcMotor.get("spoolLeft");
         spoolLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
@@ -119,14 +99,14 @@ public class UpdatedDriveProgram extends LinearOpMode {
         spoolRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         spoolRightMotor.setPower(1);
 
-        // Yield until the program starts
+        // Yield until the program starts.
         waitForStart();
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
             // Claw Information
             //
-            // If x is pressed, the claw will either open or close based on it's current state.
+            // It literally opens and closes the claw... What do you expect?
 
             if (gamepad1.x && !pressXDebounce) {
                 pressXDebounce = true;
@@ -144,7 +124,10 @@ public class UpdatedDriveProgram extends LinearOpMode {
 
             // Claw Pivot Information
             //
-            // If y is pressed, the claw pivot will pivot based on it's current state.
+            // Rotates to claw depending on button press.
+            //
+            // Make sure the Servo's 0 and 1 positions are programmed using a
+            // Servo programmer.
 
             if (gamepad1.y && !pressYDebounce) {
                 pressYDebounce = true;
@@ -162,42 +145,37 @@ public class UpdatedDriveProgram extends LinearOpMode {
 
             // Linear Slides Spool Information
             //
-            // If right trigger is held down, the linear slide will go up.
-            // If the left trigger is held down, the linear slide will go down.
-            // If none are pressed, the motor stays in it's current position, essentially stationary.
+            // This portion is programmed with the thought of the linear slides
+            // being in it's initial position ( all the way down ) with starting
+            // the bot.
 
-            spoolLeftMotor.setPower(SPOOL_MOTOR_POWER);
-            spoolRightMotor.setPower(SPOOL_MOTOR_POWER);
             if (gamepad1.right_trigger > 0) {
                 if ((currentSpoolPosition + SPOOL_INCREMENT) >= MAX_SPOOL_POSITION) {
                     currentSpoolPosition = MAX_SPOOL_POSITION;
                 } else {
                     currentSpoolPosition += SPOOL_INCREMENT;
                 }
-                // might need to add if statements to set power to 0 when at current spool position
-                spoolLeftMotor.setTargetPosition(currentSpoolPosition);
-                spoolRightMotor.setTargetPosition(currentSpoolPosition);
             } else if (gamepad1.left_trigger > 0) {
                 if ((currentSpoolPosition - SPOOL_INCREMENT) <= MIN_SPOOL_POSITION) {
                     currentSpoolPosition = MIN_SPOOL_POSITION;
                 } else {
                     currentSpoolPosition -= SPOOL_INCREMENT;
                 }
-
-                spoolLeftMotor.setTargetPosition(currentSpoolPosition);
-                spoolRightMotor.setTargetPosition(currentSpoolPosition);
-            } else {
-                spoolLeftMotor.setPower(STATIONARY_SPOOL_POWER);
-                spoolRightMotor.setPower(STATIONARY_SPOOL_POWER);
             }
+            spoolLeftMotor.setPower(CalculatePIDPower(spoolLeftMotor, currentSpoolPosition));
+            spoolLeftMotor.setTargetPosition(currentSpoolPosition);
+
+            spoolRightMotor.setPower(CalculatePIDPower(spoolRightMotor, currentSpoolPosition));
+            spoolRightMotor.setTargetPosition(currentSpoolPosition);
 
             // --------------------------------------------------------------------
 
             // Arm Information
             //
-            // If right bumper is held down, the arm goes up.
-            // If left bumper is held down, the arm goes down.
-            // If none are pressed, the arm stays stationary.
+            // The servo still tweaks out a bit when going to it's position
+            // but I guess it's fine?
+            //
+            // Too bad that PIDs don't work/exist for Servos... or do they...?
 
             if (gamepad1.right_bumper) {
                 if ((currentArmPosition + ARM_INCREMENT) >= MAX_ARM_POSITION) {
@@ -205,17 +183,15 @@ public class UpdatedDriveProgram extends LinearOpMode {
                 } else {
                     currentArmPosition += ARM_INCREMENT;
                 }
-                armRightServo.setPosition(currentArmPosition);
-                armLeftServo.setPosition(currentArmPosition);
             } else if (gamepad1.left_bumper) {
                 if ((currentArmPosition - ARM_INCREMENT) <= MIN_ARM_POSITION) {
                     currentArmPosition = MIN_ARM_POSITION;
                 } else {
                     currentArmPosition -= ARM_INCREMENT;
                 }
-                armRightServo.setPosition(currentArmPosition);
-                armLeftServo.setPosition(currentArmPosition);
             }
+            armRightServo.setPosition(currentArmPosition);
+            armLeftServo.setPosition(currentArmPosition);
 
             // --------------------------------------------------------------------
 
@@ -239,13 +215,16 @@ public class UpdatedDriveProgram extends LinearOpMode {
             topRightMotor.setPower(topRightPower);
             bottomRightMotor.setPower(bottomRightPower);
 
+            // --------------------------------------------------------------------
+
             // Telemetry data
-            /*
+            //
+            // Outputs the data of each variable
+
             telemetry.addData("GamepadX", gamepad1.x);
             telemetry.addData("Opened", remadeClawServo.getPosition() == 0);
             telemetry.addData("Position", remadeClawServo.getPosition());
             telemetry.addData("------------", "------------");
-            */
 
             telemetry.addData("RightBumper", gamepad1.right_bumper);
             telemetry.addData("LeftBumper", gamepad1.left_bumper);
